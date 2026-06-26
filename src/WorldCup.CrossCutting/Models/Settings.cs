4752eb831f626a6f1b00b686d672129506a768a0
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-06-26 10:36:24 -0300

    feat: Created configuration classes.

---
+++ src/WorldCup.CrossCutting/Models/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace WorldCup.CrossCutting.Models
+{
+    public interface ISettings
+    {
+        public WorldCupSettings WorldCupSettings { get; }
+    }
+
+    public class Settings : ISettings
+    {
+
+        public required WorldCupSettings WorldCupSettings { get; set; }
+    }
+}
