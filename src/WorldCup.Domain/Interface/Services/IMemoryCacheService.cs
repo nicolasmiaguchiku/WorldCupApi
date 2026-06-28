730f4f6a50466cbc7b6ef11eed834e2b402848c9
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-06-28 00:26:26 -0300

    feat: This commit implements the logic to use the in-memory cache

---
+++ src/WorldCup.Domain/Interface/Services/IMemoryCacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace WorldCup.Domain.Interface.Services
+{
+    public interface IMemoryCacheService
+    {
+        T? Get<T>(string ke);
+        void Set<T>(string key, T value);
+        void Remove(string key);
+    }
+}
--- src/WorldCup.Infrastructure/Services/WorldCupService.cs
+++ src/WorldCup.Infrastructure/Services/WorldCupService.cs
@@ -1,13 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using Flurl;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using WorldCup.Domain.Errors;
 using WorldCup.Domain.Interface.Services;
 using WorldCup.Domain.Models;
-using Flurl;
-using WorldCup.Domain.Errors;
 
 namespace WorldCup.Infrastructure.Services
 {
-    public class WorldCupService(IHttpClientFactory httpClientFactory) : IWorldCupServices
+    public class WorldCupService(IHttpClientFactory httpClientFactory, IMemoryCacheService memoryCacheService) : IWorldCupServices
     {
         public HttpClient httpClient = httpClientFactory.CreateClient("worldcup");
 
@@ -19,12 +19,21 @@ namespace WorldCup.Infrastructure.Services
 
         public async Task<Result<MatchesResponse>> GetListMatchAsync(CancellationToken cancellationToken)
         {
+            MatchesResponse? matches;
+
+            matches = memoryCacheService.Get<MatchesResponse>("matchesList");
+
+            if (matches is not null)
+            {
+                return Result<MatchesResponse>.Success(matches);
+            }
+
             Url url = httpClient.BaseAddress
                 .AppendPathSegment("get")
                 .AppendPathSegment("games");
 
             HttpResponseMessage response = await httpClient.GetAsync(url, cancellationToken);
-            string content = await response.Content.ReadAsStringAsync();
+            string content = await response.Content.ReadAsStringAsync(cancellationToken);
 
             if (!response.IsSuccessStatusCode)
             {
@@ -34,7 +43,9 @@ namespace WorldCup.Infrastructure.Services
                 return Result<MatchesResponse>.Failure(MatchesErrors.GetMatchesError);
             }
 
-            MatchesResponse matches = JsonConvert.DeserializeObject<MatchesResponse>(content)!;
+            matches = JsonConvert.DeserializeObject<MatchesResponse>(content)!;
+
+            memoryCacheService.Set<MatchesResponse>("matchesList", matches);
 
             return Result<MatchesResponse>.Success(matches);
         }
